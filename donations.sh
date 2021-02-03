f3881423d016551b4957002fe16bad04633a9e1d
sburns1369 <kiwishoepolish@yahoo.com>
2021-02-02 22:54:38 -0500

    testing

---
+++ donations.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+BLUE='\033[0;96m'
+GREEN='\033[0;92m'
+RED='\033[0;91m'
+YELLOW='\033[0;93m'
+CLEAR='\033[0m'
+clear
+echo
+echo
+echo
+echo -e "Donations can be made to multiple addresses for multiple projects"
+echo
+echo -e ${BLUE}" Your patronage is apprappreciated, tipping addresses"${CLEAR}
