d85bdb3162e30f1f0cd3ac61c2e07d713b330c27
sburns1369 <kiwishoepolish@yahoo.com>
2021-02-04 17:10:08 -0500

    Update Fatcat.sh

--- Fatcat.sh
+++ Fatcat.sh
@@ -3163,7 +3163,7 @@ Function_Build_Masternode_Key_Table(){
     nodeunit=1
     Function_Start_Masternode
     sleep 20
-    until ${COINDAEMONCLI} -datadir=/home/${COINl}${nodeunit}/${COINCORE} mnsync status | grep -m 1 'IsBlockchainSynced" : true'; do
+    until ${COINDAEMONCLI} -datadir=/home/${COINl}${nodeunit}/${COINCORE} mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do
       ${COINDAEMONCLI} -datadir=/home/${COINl}${nodeunit}/${COINCORE} getblockcount
       sleep 30
     done
